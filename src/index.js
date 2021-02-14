fafcc6586728a1e1d400b17a46790924a8591f53
Jacub <jacub@jacub.xyz>
2021-02-14 18:07:48 +0100

    Discord RPC

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const clientId = '810552076304121866';
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const ipc = require('electron').ipcMain;
@@ -10,6 +11,9 @@ if (require('electron-squirrel-startup')) {
 	// eslint-disable-line global-require
 	app.quit();
 }
+const DiscordRPC = require('discord-rpc');
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+const startTimestamp = new Date();
 async function launchMonolith() {
 	var SteamLocation = await findSteam();
 	cp.spawn(`${SteamLocation}/steam.exe`, [ '-applaunch', '4000', '+connect', '208.103.169.58:27015' ]);
@@ -22,7 +26,18 @@ function getramusage() {
 	return `${awa.toFixed(2)}/${awo.toFixed(2)}GB (${Math.round(awa / awo * 100)}%)`;
 }
 
+async function setActivity() {
+	rpc.setActivity({
+		details: 'Thinking about joining Monolith',
+		state: 'Exploring menus',
+		startTimestamp,
+		largeImageKey: 'discord-asset-unicorn',
+		largeImageText: "Unicorn :O"
+	});
+}
+
 const createWindow = async () => {
+	rpc.login({ clientId });
 	// Create the browser window.
 	const mainWindow = new BrowserWindow({
 		width: 800,
@@ -37,6 +52,7 @@ const createWindow = async () => {
 	// and load the index.html of the app.
 	await mainWindow.loadFile(path.join(__dirname, 'index.min.html'));
 	mainWindow.setTitle('MonoLauncher');
+	setActivity();
 	window = mainWindow;
 };
 
@@ -44,6 +60,10 @@ const createWindow = async () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
+
+rpc.on('ready', () => {
+	console.log(rpc.user);
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -67,6 +87,11 @@ ipc.on('game-launch', function(event, arg) {
 ipc.on('request-ram', function(event, arg) {
 	window.webContents.send('ram', getramusage());
 });
+ipc.on('request-discord', (event, arg) => {
+	window.webContents.send('discord', `${rpc.user.username}#${rpc.user.discriminator}`);
+});
+
+setInterval(setActivity, 16000);
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
