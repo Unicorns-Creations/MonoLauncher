f243b51591d9ca9df9a727b259a4207adaccfda1
Jacub <jacub@jacub.xyz>
2021-02-14 17:06:49 +0100

    Change time format, fix commas

--- src/players.js
+++ src/players.js
@@ -1,5 +1,16 @@
 var gamedig = require('gamedig');
-var ms = require('ms');
+function toTimeFormat(totalSeconds) {
+	hours = Math.floor(totalSeconds / 3600);
+	totalSeconds %= 3600;
+	minutes = Math.floor(totalSeconds / 60);
+	seconds = totalSeconds % 60;
+
+	minutes = String(minutes).padStart(2, '0');
+	hours = String(hours).padStart(2, '0');
+	seconds = String(seconds).padStart(2, '0');
+
+	return `${hours}:${minutes}:${seconds}`;
+}
 async function updatePlayers() {
 	var onlinepe = document.getElementById('online-players-count');
 	var onlinetbl = document.getElementById('online-table');
@@ -10,10 +21,13 @@ async function updatePlayers() {
 	});
 	if (!state) return;
 	onlinepe.innerText = `${state.players.length}/${state.maxplayers}`;
-	var players = state.players.filter((f) => f.name && f.name.length > 1).map((f) => {
-		return `<tr> <td>${f.name}</td> <td>${ms(f.time * 1000, { long: true })}</td> </tr>`;
-	}).join("\n")
-	onlinetbl.innerHTML = `<tr> <th>Steam Name</th> <th>Playtime</th> </tr>` + players;
+	var players = state.players
+		.filter((f) => f.name && f.name.length > 1)
+		.map((f) => {
+			return `<tr> <td>${f.name}</td> <td>${toTimeFormat(Math.ceil(f.time))}</td> </tr>`;
+		})
+		.join('\n');
+	onlinetbl.innerHTML = `<tr> <th>Steam Name</th> <th>Playtime (HH:MM:SS)</th> </tr>` + players;
 }
 
 setInterval(updatePlayers, 2500);
