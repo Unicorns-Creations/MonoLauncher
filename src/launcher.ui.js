c043c736e55ef4550b9fb79cabba87aceac85477
Jacub <jacubrstnc@gmail.com>
2021-03-24 16:59:46 +0100

    haha more break stuff

---
+++ src/launcher.ui.js
@@ -0,0 +1,21 @@
+function tick() {
+	var element = React.createElement(
+		'div',
+		null,
+		React.createElement(
+			'h1',
+			null,
+			'Hello, world!'
+		),
+		React.createElement(
+			'h2',
+			null,
+			'It is ',
+			new Date().toLocaleTimeString(),
+			'.'
+		)
+	);
+	ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
