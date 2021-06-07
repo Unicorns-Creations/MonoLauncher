ccd58fff16d9f16745396b1e6e4e11ce27edc053
Jacub <jacubrstnc@gmail.com>
2021-06-08 01:14:27 +0200

    progress

--- src/tools.js
+++ src/tools.js
@@ -1,4 +1,4 @@
 function MonoTablet() {
-    window.open('monotablet.html', 'MonoTablet', 'top=0,left=0,frame=true,nodeIntegration=no,width=1045,height=645')
+    window.open('monotablet.html', 'MonoTablet', 'top=0,left=0,frame=true,nodeIntegration=true,contextIsolation=false,width=1045,height=645')
 
 }
