f25334af183e2c2d1a7933053bfb8b3a0b832d93
Jacub <38807049+JacubRSTNC@users.noreply.github.com>
2021-11-16 16:13:09 +0100

    mooore cleaning

--- src/index.js
+++ src/index.js
@@ -528,20 +528,6 @@ ipc.on('join-discord', async () => {
 	});
 });
 
-// autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-// 	const dialogOpts = {
-// 		type: 'info',
-// 		buttons: [ 'Restart', 'Later' ],
-// 		title: 'Application Update',
-// 		message: process.platform === 'win32' ? releaseNotes : releaseName,
-// 		detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-// 	};
-
-// 	dialog.showMessageBox(dialogOpts).then((returnValue) => {
-// 		if (returnValue.response === 0) autoUpdater.quitAndInstall();
-// 	});
-// });
-
 autoUpdater.on('error', (message) => {
 	console.error('There was a problem updating the application');
 	console.error(message);
