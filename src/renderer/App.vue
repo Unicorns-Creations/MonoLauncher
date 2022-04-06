26ee9e044fc8e7ecaf32b681cb6c104f3cf4c390
Jacub <38807049+JacubRSTNC@users.noreply.github.com>
2022-04-06 11:24:53 +0200

    electron-vue boilerplate

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'monolauncher'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
